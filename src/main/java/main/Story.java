c7d0237ca63c92d994eed90b10f8dc3dec86e243
GiulianoDerosas <giulianoderosas@gmail.com>
2021-06-18 18:07:06 +0400

    Formatted the story so it's easier to read in the terminal.

--- src/main/java/main/Story.java
+++ src/main/java/main/Story.java
@@ -14,6 +14,7 @@ public class Story {
         System.out.println("\n");
         System.out.println("All you seek now, is revenge. You begin planning your journey to defeat the QUANTUM CODER and rescue the hard drives!");
         System.out.println("You can then use the data to re-digitise your friends!");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -27,6 +28,7 @@ public class Story {
         System.out.println("\nAfter clambering through the room, you finally reach the server power switch.");
         System.out.println("You use all your might to flip the switch and restore power, the journey to defeat the QUANTUM CODER begins.");
         System.out.println("\nYou turn around, but the QUANTUM CODERs powers have turned the broken cabling into Electric Death Snakes, interesting.");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -38,6 +40,7 @@ public class Story {
         System.out.println("You did it! You crossed the server room and you're still alive!");
         System.out.println("As you step out the exit, you're elated as you can breathe freely. Just as you exhale, you are warped into a cosmic blockchain");
         System.out.println("\nYou feel a surge of energy and the experience you gained allows you to learn another trait!");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -51,6 +54,7 @@ public class Story {
         System.out.println("Luckily you know that every once in a while a hyper trader comes across this network.");
         System.out.println("You also know exactly where the QUANTUM CODER is, the surge of energy blessed you with a vision.");
         System.out.println("\nBut you have to cross this chaotic universe first...");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -63,6 +67,7 @@ public class Story {
         System.out.println("You are just a few dimensions away from your final destination; the Starbucks of Hate!");
         System.out.println("You know that you probably can't rest there, so you take a while to restore some health.");
         System.out.println("\nYou are warped to a new dimension. After all you've seen, you feel empowered to learn another trait.");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -76,6 +81,7 @@ public class Story {
         System.out.println("You're disguised as a hipster and enter the store. You don't know how much time you have until someone discovers you!");
         System.out.println("The github stickers are starting to peel away from your macbook, fear begins to engulf you");
         System.out.println("All you can do know is fight for your life and pray to come out as the winner...");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -89,6 +95,7 @@ public class Story {
         System.out.println("As you stand in front of the door to his throne room, you gain a bout of confidence.");
         System.out.println("You recall your lost power and restore your health.");
         System.out.println("You drink a super-charged espresso and learn a new trait before entering the throne room.");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
@@ -102,6 +109,7 @@ public class Story {
         System.out.println("He takes out the RAMBLADE-4400Mhz, the fastest weapon known to man.");
         System.out.println("All you can do know is fight for your life. You think about why his throne room is called a throne room...");
         System.out.println("\nThen you realise your in the Starbucks of Hates' toilet. Things are about to get messy.");
+        System.out.println("\n");
         GameLogic.pressToContinue();
     }
 
