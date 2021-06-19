d3f367b09e843398dad4f4793d7978a8a7869a53
GiulianoDerosas <giulianoderosas@gmail.com>
2021-06-19 15:50:50 +0400

    Altered some enemy attack logic as often player was not taking enough damage.

--- src/main/java/main/Enemy.java
+++ src/main/java/main/Enemy.java
@@ -12,9 +12,10 @@ public class Enemy extends Character{
 
 
 //    Enemy specific methods
+//    Changed 4 to 2 in this method as the enemies were doing consistently low damage.
     @Override
     public int attack() {
-        return (int) (Math.random()*(playerXp/4 + 1) + xp/4 + 3);
+        return (int) (Math.random()*(playerXp/2 + 1) + xp/4 + 3);
     }
 
     @Override
